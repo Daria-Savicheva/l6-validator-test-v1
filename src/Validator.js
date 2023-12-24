8db93f20454451d9a9761496edce0705e2d42f65
Daria Savicheva <monte111@bk.ru>
2023-12-24 10:05:51 +0300

    steps 4,5

--- src/Validator.js
+++ src/Validator.js
@@ -1,5 +1,6 @@
 import ArraySchema from './ArraySchema.js';
 import NumberSchema from './NumberSchema.js';
+import ObjectSchema from './ObjectSchema.js';
 
 export default class Validator {
   number() {
@@ -11,4 +12,20 @@ export default class Validator {
     const validator = (value) => Array.isArray(value);
     return new ArraySchema([validator]);
   }
+
+  object() {
+    return new ObjectSchema();
+  }
 }
+
+const v = new Validator();
+
+// Позволяет описывать валидацию для свойств объекта
+const schema = v.object().shape({
+  num: v.number(),
+  // теперь, при валидации объекта с ключом lesson, значение этого ключа пройдет валидацию в соответствии с текущими методами
+  array: v.array().allIntegers(),
+});
+
+schema.isValid({ num: 54, array: [1, 2, 3, 5, 65, 2] }); // true
+schema.isValid({ num: 2, array: [1, 2, '4'] }); // false
